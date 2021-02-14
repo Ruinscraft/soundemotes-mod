f1c141fd3cc3f3d9d2cb3927b8e3fd81c1cbb2bb
Dexton Anderson <contact@dextonanderson.com>
2021-02-13 22:05:14 -0600

    Remove debug

--- src/main/java/com/ruinscraft/soundemotes/SoundEmotesMod.java
+++ src/main/java/com/ruinscraft/soundemotes/SoundEmotesMod.java
@@ -1,9 +1,6 @@
 package com.ruinscraft.soundemotes;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.client.MinecraftClient;
-
-import java.util.concurrent.CompletableFuture;
 
 public class SoundEmotesMod implements ModInitializer {
 
@@ -11,21 +8,6 @@ public class SoundEmotesMod implements ModInitializer {
     public void onInitialize() {
         NetworkUtil.init();
         SoundUtil.init();
-
-        CompletableFuture.runAsync(() -> {
-            while (true) {
-                if (MinecraftClient.getInstance().world != null) {
-                    MinecraftClient.getInstance().player.sendChatMessage("uh no");
-                }
-
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
     }
 
 }
