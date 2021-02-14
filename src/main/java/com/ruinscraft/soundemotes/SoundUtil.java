f1c840759783e576cf093cd7a543ae5a6e34fc4c
Dexton Anderson <contact@dextonanderson.com>
2021-02-13 21:07:43 -0600

    Release sound sources

--- src/main/java/com/ruinscraft/soundemotes/SoundUtil.java
+++ src/main/java/com/ruinscraft/soundemotes/SoundUtil.java
@@ -7,15 +7,47 @@ import net.minecraft.util.math.Vec3d;
 import java.lang.reflect.Field;
 import java.net.URL;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public final class SoundUtil {
 
     private static Map<String, StaticSound> soundCache;
+    private static List<Source> activeSources;
 
     static {
         soundCache = new ConcurrentHashMap<>();
+        activeSources = new ArrayList<>();
+
+        new Thread(() -> {
+            MinecraftClient client = MinecraftClient.getInstance();
+
+            // Source cleanup thread
+            while (client.isRunning()) {
+                Iterator<Source> iterator = activeSources.iterator();
+                while (iterator.hasNext()) {
+                    Source source = iterator.next();
+                    if (client.world == null || source.isStopped()) {
+                        try {
+                            getSoundEngine().release(source);
+                            source.close();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        iterator.remove();
+                    }
+                }
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     public static void playSound(PlayedSoundEmote playedSoundEmote) {
@@ -51,19 +83,26 @@ public final class SoundUtil {
     }
 
     private static void playStaticSound(StaticSound staticSound, Vec3d position) throws Exception {
-        MinecraftClient client = MinecraftClient.getInstance();
-        SoundManager soundManager = client.getSoundManager();
-        if (soundManager == null) return;
-        Field soundSystemField = soundManager.getClass().getDeclaredField("field_5590");
-        soundSystemField.setAccessible(true);
-        SoundSystem soundSystem = (SoundSystem) soundSystemField.get(soundManager);
-        Field soundEngineField = soundSystem.getClass().getDeclaredField("field_18945");
-        soundEngineField.setAccessible(true);
-        SoundEngine soundEngine = (SoundEngine) soundEngineField.get(soundSystem);
-        Source source = soundEngine.createSource(SoundEngine.RunMode.STATIC);
+        Source source = getSoundEngine().createSource(SoundEngine.RunMode.STATIC);
         source.setBuffer(staticSound);
         source.setPosition(position);
         source.play();
+        activeSources.add(source);
+    }
+
+    public static SoundEngine getSoundEngine() throws Exception {
+        MinecraftClient client = MinecraftClient.getInstance();
+        SoundManager soundManager = client.getSoundManager();
+        if (soundManager == null) return null;
+        Field soundSystemField = soundManager.getClass().getDeclaredField("field_5590");
+//        Field soundSystemField = soundManager.getClass().getDeclaredField("soundSystem");
+        soundSystemField.setAccessible(true);
+        SoundSystem soundSystem = (SoundSystem) soundSystemField.get(soundManager);
+        Field soundEngineField = soundSystem.getClass().getDeclaredField("field_18945");
+//        Field soundEngineField = soundSystem.getClass().getDeclaredField("soundEngine");
+        soundEngineField.setAccessible(true);
+        SoundEngine soundEngine = (SoundEngine) soundEngineField.get(soundSystem);
+        return soundEngine;
     }
 
 }
